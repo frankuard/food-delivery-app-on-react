76f1c0d5c3ae3f3753ba82ea216951543f367300
Roshan Karki <roshankarki4956@gmail.com>
2026-08-01 00:33:00 +0545

    finishing up the project of the login popup and its design now mocing into the cart section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './components/NavBar/NavBar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart.jsx'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
 import Footer from './components/Footer/Footer.jsx'
+import LoginPopup from './components/LoginPopup/LoginPopup.jsx'
 
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false)
+
   return (
     <> 
+    {showLogin ?<LoginPopup setShowLogin={setShowLogin} />: <></>}
     <div className=' app'>
-      <NavBar />
+      <NavBar setShowLogin={setShowLogin} />
 
       <Routes>
         <Route path='/' element={<Home />} />
