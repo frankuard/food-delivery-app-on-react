662862fc769104a71e9f3099b23c68447623b197
Roshan Karki <roshankarki4956@gmail.com>
2026-08-01 00:42:25 +0545

    integrating routes for the cart and the main home logo section

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,8 +10,9 @@ const NavBar = ({setShowLogin}) => {
 
   return (
     <div className=' navbar'>
-
+        <Link to='/'>
         <img src={assets.logo} alt="" className='logo' />
+        </Link>
         <ul className="navbar-menu"> 
             <Link to='/' onClick={() => setMenu("Home")} className={menu==="Home" ? "active" : ""}>Home</Link>
             <a href='#explore-menu' onClick={() => setMenu("Menu")} className={menu==="Menu" ? "active" : ""}>Menu</a>
@@ -23,7 +24,9 @@ const NavBar = ({setShowLogin}) => {
         <div className='navbar-right'>
             <img src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
+                <Link to='/cart'>
                 <img src={assets.basket_icon} alt="" />
+                </Link>
                 <div className="dot"></div>
             </div>
             <button onClick={() =>setShowLogin(true)}>Sign In</button>
