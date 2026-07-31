2272a9c1c44ea6cad8d395f8f20c94a3d447e1b0
Roshan Karki <roshankarki4956@gmail.com>
2026-08-01 01:58:17 +0545

    adding the logic into the cart section of the project and also making the recipet portion of the prj and finally compleiting everything

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,12 @@
 import React, { useContext } from 'react'
 import { StoreContext } from '../../context/StoreContext'
 import './Cart.css'
+import { useNavigate } from 'react-router-dom'
 const Cart = () => {
 
   const { cartItems, food_list, removeFromCart, getTotalCartAmount} = useContext(StoreContext)
+
+  const navigate = useNavigate();
 
   return (
     <div className='cart' id='cart'>
@@ -60,7 +63,7 @@ const Cart = () => {
               <b>{getTotalCartAmount()+2}</b>
             </div>
           </div>
-          <button>PROCEED TO CHECKOUT</button>
+          <button onClick={() => navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
           <div>
