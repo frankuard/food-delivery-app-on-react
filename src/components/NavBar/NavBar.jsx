d3d9c5a3a163c06c16531e7109b61c04db7f2e08
Roshan Karki <roshankarki4956@gmail.com>
2026-07-31 23:38:07 +0545

    making the tags into link tags and making the scroll feel smooth

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './NavBar.css'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 
 
 const NavBar = () => {
@@ -12,10 +13,10 @@ const NavBar = () => {
 
         <img src={assets.logo} alt="" className='logo' />
         <ul className="navbar-menu"> 
-            <li onClick={() => setMenu("Home")} className={menu==="Home" ? "active" : ""}>Home</li>
-            <li onClick={() => setMenu("Menu")} className={menu==="Menu" ? "active" : ""}>Menu</li>
-            <li onClick={() => setMenu("Mobile-App")} className={menu==="Mobile-App" ? "active" : ""}>Mobile-App</li>
-            <li onClick={() => setMenu("Contact-Us")} className={menu==="Contact-Us" ? "active" : ""}>Contact Us</li>
+            <Link to='/' onClick={() => setMenu("Home")} className={menu==="Home" ? "active" : ""}>Home</Link>
+            <a href='#explore-menu' onClick={() => setMenu("Menu")} className={menu==="Menu" ? "active" : ""}>Menu</a>
+            <a href='#app-download' onClick={() => setMenu("Mobile-App")} className={menu==="Mobile-App" ? "active" : ""}>Mobile-App</a>
+            <a href='#footer' onClick={() => setMenu("Contact-Us")} className={menu==="Contact-Us" ? "active" : ""}>Contact Us</a>
             
         </ul>
 
